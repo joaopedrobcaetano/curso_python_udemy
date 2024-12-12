e7b8ec2b5862dd762500367e7f08e9f038189aec
joaopedrobcaetano <joaopedrobcaetano@gmail.com>
2024-12-12 18:22:58 -0300

    testando comentários e docstrings

---
+++ aula1.py
@@ -0,0 +1,13 @@
+# Este sinal permite escrever um comentário. Pode ser em cima do texto
+print(123) # Na frente
+# Ou abaixo, mas é melhor evitar
+print(456)
+
+"""
+Isto são DocStrings.
+Não são comentários, mas servem para escrever notas em mais de uma linha de código
+"""
+
+'''
+Pode ser usado com aspas simples também
+'''
