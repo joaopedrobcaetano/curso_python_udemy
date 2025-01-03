24a509bdccc0178e2e8cf98cea3a1a4e77941cf1
joaopedrobcaetano <joaopedrobcaetano@gmail.com>
2025-01-02 21:21:08 -0300

    testando f-strings

--- aula12.py
+++ aula12.py
@@ -2,4 +2,9 @@ nome = 'João Pedro'
 altura = 1.96
 peso = 88
 imc = peso / (altura * altura)
+
 print(nome, 'tem', altura, 'de altura, pesa', peso, 'quilos e seu IMC é', imc)
+
+testando_fstrings = f'{nome} tem {altura:.2f} de altura, pesa {peso} quilos e seu IMC é {imc:.2f}.'
+
+print(testando_fstrings)
